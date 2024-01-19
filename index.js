20674031deb348c05793dea4b6fdd5722f708434
khj33398 <khj33398@naver.com>
2024-01-19 16:25:56 +0900

    로그인 출력

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
     index.html 에서 사용되는 js
 */
 
-function login(username, password) {
+function login(username, password, flag) {
     //login 처리
     if(username != null && password != null && flag) {
         //username값과 FLAG 값을 체크
+        console.log("로그인 처리" + username + " " + password + " " + flag);
     }
     return true;
 }
